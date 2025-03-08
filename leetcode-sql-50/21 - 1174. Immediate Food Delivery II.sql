56ee88d22c5d9df20b2e8770ce060fff6f4db24a
Josue Veliz <josuedemetriovelizescobar@gmail.com>
2025-03-07 20:35:01 -0400

    Improve solution in 21

--- leetcode-sql-50/21 - 1174. Immediate Food Delivery II.sql
+++ leetcode-sql-50/21 - 1174. Immediate Food Delivery II.sql
@@ -11,4 +11,16 @@ select
     ROUND((
         avg((order_date = customer_pref_delivery_date)::int)
     )::numeric * 100, 2) as immediate_percentage
-from delivery_first_order 
+from delivery_first_order;
+
+-- Better solution:
+SELECT 
+    ROUND(
+        AVG((order_date = customer_pref_delivery_date)::int) * 100, 2
+    ) AS immediate_percentage
+FROM Delivery
+WHERE (customer_id, order_date) IN (
+    SELECT customer_id, MIN(order_date) 
+    FROM Delivery
+    GROUP BY customer_id
+);
